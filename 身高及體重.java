1d903e0d27dbe4040f7ef84a1408ca51601c0425
107021159 <44196562+107021159@users.noreply.github.com>
2018-10-17 00:00:21 +0800

    Add files via upload

---
+++ 身高及體重.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scn = new Scanner(System.in);
+	int cm = scn.nextInt();
+	int kg = scn.nextInt();
+	double a = cm/2.54;
+	double b = kg/0.454;
+	System.out.println(a);
+	System.out.println(b);
+
+    }
+}
