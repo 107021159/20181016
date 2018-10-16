bf1d1f1a950debde4a24fd4c8bfd62827c602770
107021159 <44196562+107021159@users.noreply.github.com>
2018-10-17 00:01:47 +0800

    Add files via upload

---
+++ 和差及平均成績.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int h1 = 8;
+        int h2 = 9;
+        System.out.println(h1+h2);
+        System.out.println(h1-h2);
+        System.out.println(h1*h2);
+    }
+}
