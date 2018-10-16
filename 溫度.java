bd6df15fdc496fe8b496d038a35c758103041eb9
107021159 <44196562+107021159@users.noreply.github.com>
2018-10-17 00:07:03 +0800

    Add files via upload

---
+++ 溫度.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float c = scn.nextFloat();
+        float f = c*9/5+32;
+        System.out.println(f);
+    }
+}
